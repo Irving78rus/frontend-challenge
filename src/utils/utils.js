5524cae13d304884c058f08b8d2d013ad50f5ad8
Irving78rus <isakov911@mail.ru>
2022-05-31 13:26:33 +0300

    Updates

--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const getItemFromLocalStorage=(nameItem)=>{
-  return  JSON.parse(localStorage.getItem(nameItem)) 
-}
-
-export const setItemToLocalStorage=(nameItem,item)=>{
-    return localStorage.setItem(nameItem, JSON.stringify(item));
-}
